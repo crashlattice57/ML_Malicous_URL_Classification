ed503627b4a74a74afd67d9385335407258bff5c
crashlattice57 <130922722+crashlattice57@users.noreply.github.com>
2023-04-22 13:04:46 -0400

    Used Pipeline of Scaler and RF

---
+++ Testing/clean_data.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+import re
+from sklearn.preprocessing import StandardScaler
+
+df = pd.read_csv("malicious_phish.csv")
+
+
+def is_ip_address_present(s):
+    ipv4_pattern = r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b"
+    ipv6_pattern = r"\b([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}\b"
+    ipv4_match = re.search(ipv4_pattern, s)
+    ipv6_match = re.search(ipv6_pattern, s)
+    if ipv4_match or ipv6_match:
+        return 1
+    else:
+        return 0
+
+
+def query_domain_length(query):
+    try:
+        length = len(query.split('.')[-2])
+    except:
+        length = 0
+    return length
+
+
+def has_malicious_file_extension(s):
+    pattern = r"(\.exe|\.dll|\.bat|\.cmd|\.msi|\.vbs|\.ps1|\.psm1|\.js|\.jse|\.wsh|\.wsf|\.hta|\.scr|\.pif|\.cpl" \
+              r"|\.ade|\.adp|\.bas|\.chm|\.cmd|\.com|\.crt|\.csh|\.hlp|\.inf|\.ins|\.isp|\.job|\.js|\.jse|\.lnk|\.mda" \
+              r"|\.mdb|\.mde|\.mdt|\.mdw|\.mdz|\.msc|\.msi|\.msp|\.mst|\.nws|\.pcd|\.prf|\.reg|\.scf|\.shb|\.shs" \
+              r"|\.tmp|\.url|\.vb|\.vbe|\.vbs|\.wsc|\.wsf|\.wsh)$"
+    match = re.search(pattern, s, re.IGNORECASE)
+    return 1 if bool(match) else 0
+
+
+def has_shortened_link(url):
+    shortening_services = ["bit.ly", "t.co", "tinyurl.com", "ow.ly", "goo.gl", "is.gd", "buff.ly", "adcrun.ch",
+                           "qr.net", "adf.ly", "bc.vc", "ow.ly", "po.st", "tr.im", "v.gd", "x.co", "tiny.cc",
+                           "tinyurl.co.uk", "tinyurl.de", "tinyurl.fr", "tinyurl.pl", "tinylink.in", "tinyuri.ca",
+                           "tinyurl.dk", "url.ie", "zi.pe"]
+    for service in shortening_services:
+        pattern = fr"\b{service}\b"
+        if re.search(pattern, url, re.IGNORECASE):
+            return 1
+    return 0
+
+
+def preprocess_input(input_data):
+    """
+    Takes in a string url as input and does preprocessing of all steps to get ready for prediction
+    """
+    df = pd.DataFrame({"url": [input_data]})
+
+    df["url_len"] = df["url"].apply(lambda x: len(x))
+    df["url_parts"] = df["url"].apply(lambda x: len(x.split(".")))
+    df["suffix_len"] = df["url"].apply(lambda x: len(x.split(".")[-1]))
+    df["domain_len"] = df["url"].apply(query_domain_length)
+    df["ip_present"] = df["url"].apply(is_ip_address_present)
+    df["file_extension"] = df["url"].apply(has_malicious_file_extension)
+    df["shorted_link"] = df["url"].apply(has_shortened_link)
+
+    special_char = ["%", "=", "/", "?", ":", "+", "@", "&", "#", "<", ">", "^"]
+    for special in special_char:
+        df[f'{special}_count'] = df["url"].apply(lambda x: x.count(special))
--- Testing/test2.py
+++ Testing/test2.py
@@ -5,8 +5,9 @@ Spyder Editor
 This is a temporary script file.
 """
 
-import URL_Classification
+
 import pandas as pd
+import re
 from sklearn.utils import resample
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -17,10 +18,63 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV
 from urllib.parse import urlparse
 from sklearn.metrics import confusion_matrix,classification_report,plot_confusion_matrix,accuracy_score
+from sklearn.pipeline import Pipeline
 
-df = pd.read_csv("malicious_phish.csv")
+df = pd.read_csv("C:\\Users\\damia\\Documents\\GitHub\\ML_Malicous_URL_Classification\\malicious_phish.csv")
 
-"""
+def query_domain_length(query):
+    try:
+        length = len(query.split('.')[-2])
+    except:
+        length = 0
+    return length
+
+
+def is_ip_address_present(s):
+    ipv4_pattern = r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b"
+    ipv6_pattern = r"\b([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}\b"
+    ipv4_match = re.search(ipv4_pattern, s)
+    ipv6_match = re.search(ipv6_pattern, s)
+    if ipv4_match or ipv6_match:
+        return 1
+    else:
+        return 0
+    
+def extract_url_attributes(url):
+    attributes = ['scheme', 'netloc', 'path', 'params', 'query', 'fragment']
+    parsed = urlparse(url)
+    result = {}
+    for attribute in attributes:
+        result[f"{attribute}_present"] = 1 if getattr(parsed, attribute) != '' else 0
+    return result
+for attribute in ['scheme', 'netloc', 'path', 'params', 'query', 'fragment']:
+    df[f"{attribute}_present"] = df['url'].apply(lambda x: extract_url_attributes(x)[f"{attribute}_present"])
+
+
+def has_malicious_file_extension(s):
+    pattern = r"(\.exe|\.dll|\.bat|\.cmd|\.msi|\.vbs|\.ps1|\.psm1|\.js|\.jse|\.wsh|\.wsf|\.hta|\.scr|\.pif|\.cpl" \
+              r"|\.ade|\.adp|\.bas|\.chm|\.cmd|\.com|\.crt|\.csh|\.hlp|\.inf|\.ins|\.isp|\.job|\.js|\.jse|\.lnk|\.mda" \
+              r"|\.mdb|\.mde|\.mdt|\.mdw|\.mdz|\.msc|\.msi|\.msp|\.mst|\.nws|\.pcd|\.prf|\.reg|\.scf|\.shb|\.shs" \
+              r"|\.tmp|\.url|\.vb|\.vbe|\.vbs|\.wsc|\.wsf|\.wsh)$"
+    match = re.search(pattern, s, re.IGNORECASE)
+    return 1 if bool(match) else 0
+
+
+def has_shortened_link(url):
+    shortening_services = ["bit.ly", "t.co", "tinyurl.com", "ow.ly", "goo.gl", "is.gd", "buff.ly", "adcrun.ch",
+                           "qr.net", "adf.ly", "bc.vc", "ow.ly", "po.st", "tr.im", "v.gd", "x.co", "tiny.cc",
+                           "tinyurl.co.uk", "tinyurl.de", "tinyurl.fr", "tinyurl.pl", "tinylink.in", "tinyuri.ca",
+                           "tinyurl.dk", "url.ie", "zi.pe"]
+    for service in shortening_services:
+        pattern = fr"\b{service}\b"
+        if re.search(pattern, url, re.IGNORECASE):
+            return 1
+    return 0
+
+
+
+
+
 # Define the value to use as the reference for the number of samples
 reference_value = "malware"
 num_samples = df["type"].value_counts()[reference_value]
@@ -43,18 +97,19 @@ for value in df["type"].unique():
 
 
 df = df_resampled
-"""
 
-df['type'] = df['type'].apply(lambda x: 'benign' if x == 'benign' else 'malicious')
+
+#df['type'] = df['type'].apply(lambda x: 'benign' if x == 'benign' else 'malicious')
 df["url_len"] = df["url"].apply(lambda x: len(x))
 df["url_parts"] = df["url"].apply(lambda x: len(x.split(".")))
 df["suffix_len"] = df["url"].apply(lambda x: len(x.split(".")[-1]))
-df["domain_len"] = df["url"].apply(URL_Classification.query_domain_length)
+df["domain_len"] = df["url"].apply(query_domain_length)
 df['digit_count'] = df['url'].apply(lambda s: sum(c.isdigit() for c in s))
 df['alpha_count'] = df['url'].apply(lambda s: sum(c.isalpha() for c in s))
-df["ip_present"] = df["url"].apply(URL_Classification.is_ip_address_present)
-df["file_extension"] = df["url"].apply(URL_Classification.has_malicious_file_extension)
-df["shorted_link"] = df["url"].apply(URL_Classification.has_shortened_link)
+df["ip_present"] = df["url"].apply(is_ip_address_present)
+df["file_extension"] = df["url"].apply(has_malicious_file_extension)
+df["shorted_link"] = df["url"].apply(has_shortened_link)
+df["url_attributes"] = df["url"].apply(extract_url_attributes)
 special_chars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', '\\', ';', ':', '\'', '\"', ',', '.', '<', '>', '/', '?']
 
 special_char_counts = []
@@ -70,16 +125,10 @@ for special in special_chars:
 df['special_chars_count'] = [counts['special_chars_count'] for counts in special_char_counts]
 
 
+#df.columns
 
-def extract_url_attributes(url):
-    attributes = ['scheme', 'netloc', 'path', 'params', 'query', 'fragment']
-    parsed = urlparse(url)
-    result = {}
-    for attribute in attributes:
-        result[f"{attribute}_present"] = 1 if getattr(parsed, attribute) != '' else 0
-    return result
-for attribute in ['scheme', 'netloc', 'path', 'params', 'query', 'fragment']:
-    df[f"{attribute}_present"] = df['url'].apply(lambda x: extract_url_attributes(x)[f"{attribute}_present"])
+
+
 
 
 X = df.drop(["type","url"],axis=1)
@@ -88,10 +137,29 @@ y = df["type"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
 
 
-    
-rfc = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
+#scaler = StandardScaler()   
+#rfc = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
 
-rfc.fit(X_train, y_train)
+pipeline = Pipeline([
+    ('scaler', StandardScaler()),
+    ('random_forest', RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42))
+])
+
+
+pipeline.fit(X_train,y_train)
+
+scaler = pipeline.named_steps["scaler"]
+
+
+y_pred = pipeline.predict(X_test)
+
+plot_confusion_matrix(pipeline,X_test,y_test)
+
+print(classification_report(y_test,y_pred))
+
+
+
+
 
 
 
@@ -106,13 +174,13 @@ def preprocess_input(input_data):
     df["url_len"] = df["url"].apply(lambda x: len(x))
     df["url_parts"] = df["url"].apply(lambda x: len(x.split(".")))
     df["suffix_len"] = df["url"].apply(lambda x: len(x.split(".")[-1]))
-    df["domain_len"] = df["url"].apply(URL_Classification.query_domain_length)
+    df["domain_len"] = df["url"].apply(query_domain_length)
     df['digit_count'] = df['url'].apply(lambda s: sum(c.isdigit() for c in s))
     df['alpha_count'] = df['url'].apply(lambda s: sum(c.isalpha() for c in s))
-    df["ip_present"] = df["url"].apply(URL_Classification.is_ip_address_present)
-    df["file_extension"] = df["url"].apply(URL_Classification.has_malicious_file_extension)
-    df["shorted_link"] = df["url"].apply(URL_Classification.has_shortened_link)
-
+    df["ip_present"] = df["url"].apply(is_ip_address_present)
+    df["file_extension"] = df["url"].apply(has_malicious_file_extension)
+    df["shorted_link"] = df["url"].apply(has_shortened_link)
+    df["url_attributes"] = df["url"].apply(extract_url_attributes)
     special_chars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', '\\', ';', ':', '\'', '\"', ',', '.', '<', '>', '/', '?']
     
     special_char_counts = []
@@ -126,17 +194,11 @@ def preprocess_input(input_data):
     for special in special_chars:
         df[f'{special}_count'] = [counts[f'{special}_count'] for counts in special_char_counts]
     df['special_chars_count'] = [counts['special_chars_count'] for counts in special_char_counts]
-    def extract_url_attributes(url):
-        attributes = ['scheme', 'netloc', 'path', 'params', 'query', 'fragment']
-        parsed = urlparse(url)
-        result = {}
-        for attribute in attributes:
-            result[f"{attribute}_present"] = 1 if getattr(parsed, attribute) != '' else 0
-        return result
     for attribute in ['scheme', 'netloc', 'path', 'params', 'query', 'fragment']:
         df[f"{attribute}_present"] = df['url'].apply(lambda x: extract_url_attributes(x)[f"{attribute}_present"])
-    X = df.drop(["url","type"], axis=1)
-    return X
+    return df
+
+test = preprocess_input("hahahaha")
 
 
 rfc.predict(preprocess_input("https://chat.openai.com/"))
